cebfce1ca70ef940ebd0ae8eff3e53f58b38d813
Hugo <hugo.piard@etu.univ-nantes.fr>
2024-11-09 16:10:43 +0100

    creation du fichier sql et insertion de la premiere donne

---
+++ dat.sql
@@ -0,0 +1,13 @@
+-- Supprimer la table si elle existe déjà, pour éviter des conflits
+DROP TABLE IF EXISTS books;
+
+-- Création de la table books
+CREATE TABLE books (
+    isbn VARCHAR(17) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    date DATE
+);
+
+-- Insertion de données dans la table books
+INSERT INTO books (isbn, name, date) VALUES
+('978-2-266-15500-7', 'La Brèche', '2005-03-01');
