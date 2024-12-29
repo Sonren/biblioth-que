3f0b914e3d92acdb77f36d7fee4f341fecf6389a
Hugo <hugo.piard@etu.univ-nantes.fr>
2024-12-29 10:55:43 +0100

    feat reorganisation a the project with coponement and improve the code to create more feature

--- library/src/App.js
+++ library/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Popup from './components/Popup';
 import PrintBook from './components/printBook';
+import AddBook from './components/addBook';
 import { useState , useEffect} from 'react';
 
 
 function App() {
   const [buttonPopup, setButtonPopup] = useState(false);
   const [books, setBooks] = useState([]); // tableau contenant les reponses de requete sql
+  const [isbn, setIsbn] = useState('');
+  const [name, setName] = useState('');
+  const [date, setDate] = useState('');
+  const [message, setMessage] = useState('');
+
 
   useEffect(() => {
     fetch('http://localhost:5001/api/books')
@@ -25,15 +31,48 @@ function App() {
       });
   }, []);
 
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Empêche le rechargement de la page
+
+    // Vérification des champs
+    if (!name || !isbn ) {
+      setMessage('Tous les champs sont obligatoires');
+      return;
+    }
+    const response = await fetch('http://localhost:5001/api/books', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, isbn, date }), // Envoi des données sous forme de JSON
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      setMessage(`Livre ajouté : ${result.name}`);
+      window.location.reload(); // recharge la page actuelle
+    } else {
+      setMessage('Erreur lors de l\'ajout du livre');
+    }
+
+    // Réinitialisation des champs du formulaire
+    setName('');
+    setIsbn('');
+    setDate('');
+  }
+
   return (
     <div className="App">
       <main>
       <h1 className='hello'> Bienvenue dans la librairie ! </h1>
         <button onClick={() => setButtonPopup(true)}>Oppen button</button>
-        <h2 className='l_livre'> Liste des livres </h2>
+        <h2 className='liste livre'> Liste des livres </h2>
       </main>
 
         <PrintBook books ={books} > </PrintBook>
+
+        <AddBook rechargement={handleSubmit} date = {date} name = {name} isbn={isbn} setName={setName} setIsbn={setIsbn} setDate={setDate} message={message}></AddBook>
+
 
         <Popup trigger={buttonPopup} setTrigger={setButtonPopup}> 
           <h3> My popup </h3>
